2e9cda530806f636c96880935f26c8f7c462ff02
MD Swaley <125475348+mdswaley@users.noreply.github.com>
2023-11-20 13:58:19 +0530

    Delete Registration/src/main/java/com/example/registration/GetAttRepo.java

--- Registration/src/main/java/com/example/registration/GetAttRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.registration;
-
-import com.example.registration.Entity.TakingAttendance;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GetAttRepo extends JpaRepository<TakingAttendance,Integer> {
-}
